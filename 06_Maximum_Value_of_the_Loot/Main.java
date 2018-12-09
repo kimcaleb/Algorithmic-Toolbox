6e77296d172d1fee14e28611cbca3b3047598cc2
Caleb Kim <calebkim2005@yahoo.com>
2018-12-08 23:04:31 -0800

    Second attempt at MaxValue

--- 06_Maximum_Value_of_the_Loot/Main.java
+++ 06_Maximum_Value_of_the_Loot/Main.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+/*
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
@@ -34,10 +34,87 @@ public class Main {
         }
         return runningValue;
     }
-}
+} */
 
  
 /*Failed case #6/13: Wrong answer
 got: 3032.1773 expected: 7777.731
- (Time used: 0.82/1.50, memory used: 34521088/671088640.) */
+ (Time used: 0.82/1.50, memory used: 34521088/671088640.)
+// Bug: TreeMap cannot have duplicate keys, so this won't work for all test cases. Need another way to sort value//weight values and match their respective weight values in a separate array.  
+
+<--Attempt 2-->*/
+ 
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int x = scn.nextInt();
+        int y = scn.nextInt();
+        List<Containers> containers = new ArrayList<>(); // Got rid of treeMap and used personal container class object that stores weightPerValue and weight and implements the comparable interface. Each object was then stored in an ArrayList and sorted. 
+        for (int i = 0; i < x; i++) {
+            double c = scn.nextDouble();
+            int d = scn.nextInt();
+            containers.add(new Containers((c/d),d));
+        }
+        Collections.sort(containers);
+        System.out.printf("%.4f", maxValue(containers, y));
+
+    }
+
+    public static double maxValue(List<Containers> a, int c) {
+        double runningValue = 0;
+        int Capacity = c;
+        for (int x = 0; x < a.size(); x++) {
+            if (Capacity <= 0) {
+                break;
+            } // If Capacity is already 0, no need to go through the other items.
+          // Need to check Capacity here in case we reach 0 Capacity while weights[x] is still greater than 0.
+            while (a.get(x).getWeight() > 0 && Capacity > 0) { // Need to check Capacity here in case we reach 0 Capacity while weights[x] is still greater than 0.
+                int min = Math.min(a.get(x).getWeight(),Capacity);
+                Capacity = Capacity - min;
+                runningValue += a.get(x).getWeightPerValue()*min;
+                a.get(x).setWeight(a.get(x).getWeight() - min);
+            }
+
+        }
+        return runningValue;
+    }
+
+}
+
+
+
+class Containers implements Comparable<Containers> {
+
+    private double weightPerValue;
+    private int weight;
+
+    public Containers(double wpv, int w) {
+        weightPerValue = wpv;
+        weight = w;
+    }
+
+    public double getWeightPerValue() {
+        return weightPerValue;
+    }
+
+    public void setWeightPerValue(double weightPerValue) {
+        this.weightPerValue = weightPerValue;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Containers o) {
+        return (int)(o.weightPerValue - this.weightPerValue);
+    }
+}
+
  
