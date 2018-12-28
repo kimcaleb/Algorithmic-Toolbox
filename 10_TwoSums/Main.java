cd59e033a3cc04e635b273a866e095872dedbbe8
Caleb Kim <calebkim2005@yahoo.com>
2018-12-27 16:00:59 -0800

    twosums

--- 10_TwoSums/Main.java
+++ 10_TwoSums/Main.java
@@ -32,6 +32,7 @@ class Main {
 	// Time complexity: 0(n2). For each element we try to find its complement by looping through the rest of the array which takes O(n) time. Space complexity: O(1).
 	
 	
+	// NOT MY SOLUTION BUT FOUND ON LEETCODE
 	//To improve our run time complexity, we need a more efficient way to check if the complement exists in the array. If the complement exists, we need to look up its index. The best way to do this is using a hash table or hash map. We reduce teh look up time from 0(n) to O(1) by trading space for speed. A hash table is built exactly for this purpose, it supports fast look up in near constant time. Near because if a collition occurred, a look up could degenerate to O(n) time. We should be fine as long as the hash function was chosen carefully. A simple implementation uses two iterations. In the first iteration, we add each element's value and its index to the table. Then, in the second iteration we check if each element's complement (target - nums[i]) exists in the table. Time complexity : O(n). We traverse the list containing n elements exactly twice. Since the hash table reduces the look up time to O(1), the time complexity is O(n). Space complexity : O(n). The extra space required depends on the number of items stored in the hash table, which stores exactly n elements. 
 	public int[] twoSumsFast(int[] nums, int targeet) {
 		Map<Integer, Integer> map = new HashMap<>();
